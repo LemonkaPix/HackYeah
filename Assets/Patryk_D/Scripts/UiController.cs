960d6ebb5ed0e654c33a262c9fd9b9bd9b896ef6
patrykduda2005 <patrykduda7@gmail.com>
2022-11-19 16:45:38 +0100

    Dodano timer

--- Assets/Patryk_D/Scripts/UiController.cs
+++ Assets/Patryk_D/Scripts/UiController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UiController : MonoBehaviour
 {
@@ -8,17 +9,27 @@ public class UiController : MonoBehaviour
     [SerializeField] GirrafeObject[] girrafeObjects;
 
     public int currentMoney;
+    public GameObject timer;
+    System.TimeSpan roundTime = new System.TimeSpan(0, 0, 0); //clock
+    private int lastSecond = 0; //pomocna zmienna do update'owania timera co sekunde
+    private string correctedSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - 1 >= lastSecond) {
+            lastSecond = Mathf.FloorToInt(Time.time);
+            roundTime = roundTime.Add(new System.TimeSpan(0, 0, 1));
+        }
+        correctedSeconds = System.Convert.ToString(roundTime.Seconds).Length < 2 ?
+            "0" + roundTime.Seconds : System.Convert.ToString(roundTime.Seconds);
+        timer.GetComponent<TMP_Text>().text = System.Convert.ToString(
+                roundTime.Minutes + ":" + correctedSeconds);
     }
 
     public void upgrade() {
